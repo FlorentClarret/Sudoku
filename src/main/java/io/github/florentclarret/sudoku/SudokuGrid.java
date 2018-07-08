a3b6c2ef413b44d39f1fcbf979d7e7527e8fd3dc
Florent Clarret <florent.clarret@gmail.com>
2018-07-08 18:24:05 +0200

    feat(sudoku) : add first methods to the sudoku interface to interact with the grid

--- src/main/java/io/github/florentclarret/sudoku/SudokuGrid.java
+++ src/main/java/io/github/florentclarret/sudoku/SudokuGrid.java
@@ -2,4 +2,14 @@ package io.github.florentclarret.sudoku;
 
 public interface SudokuGrid {
 
+    int UNDEFINED_VALUE = 0;
+
+    int getValue(final int row, final int column);
+
+    int setValue(final int row, final int column, final int value);
+
+    void reset(final int row, final int column);
+
+    void resetAll();
+
 }
